c343572c7894f6b8ce3cf9b0f6bb9662a481d567
M M <mehdifamoffice@gmail.com>
2022-11-30 22:12:53 -0500

    rotatory globe added

--- js/choropleth.js
+++ js/choropleth.js
@@ -3,34 +3,43 @@
 * * * * * * * * * * * * * */
 
 class MapVis {
-
-    constructor(parentElement, sharksData, geoData) {
+    constructor(parentElement, sharksData, geoData, data) {
         this.parentElement = parentElement;
         this.geoData = geoData;
         this.sharksData = sharksData;
+        this.data = data;
 
         // define colors
-        this.colors = ['#d1fdc7', '#66c239', 'rgba(50,154,14,0.93)', '#156004']
+        this.colors = ['#d1fdc7', '#66c239', 'rgba(50,154,14,0.93)', '#156004'];
 
-        this.initVis()
+        this.initVis();
     }
 
     initVis() {
         let vis = this;
 
-
-        vis.margin = {top: 20, right: 20, bottom: 20, left: 20};
-        vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
-        vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
+        vis.margin = { top: 20, right: 20, bottom: 20, left: 20 };
+        vis.width =
+            document.getElementById(vis.parentElement).getBoundingClientRect().width -
+            vis.margin.left -
+            vis.margin.right;
+        vis.height =
+            document.getElementById(vis.parentElement).getBoundingClientRect()
+                .height -
+            vis.margin.top -
+            vis.margin.bottom;
 
         // init drawing area
-        vis.svg = d3.select("#" + vis.parentElement).append("svg")
-            .attr("width", vis.width)
-            .attr("height", vis.height)
+        vis.svg = d3
+            .select('#' + vis.parentElement)
+            .append('svg')
+            .attr('width', vis.width)
+            .attr('height', vis.height)
             .attr('transform', `translate (${vis.margin.left}, ${vis.margin.top})`);
 
         // add title
-        vis.svg.append('g')
+        vis.svg
+            .append('g')
             .attr('class', 'title')
             .attr('id', 'map-title')
             .append('text')
@@ -39,96 +48,109 @@ class MapVis {
             .attr('text-anchor', 'middle');
 
         // add color legend
-        vis.legend = vis.svg.append("g")
+        vis.legend = vis.svg
+            .append('g')
             .attr('class', 'legend')
-            .attr('transform', `translate(${vis.width * 2.8 / 4}, ${vis.height - 20})`)
+            .attr(
+                'transform',
+                `translate(${(vis.width * 2.8) / 4}, ${vis.height - 20})`
+            );
 
-        vis.legendScale = d3.scaleLinear()
-            .range([0, 160])
-            .domain([0, 100])
+        vis.legendScale = d3.scaleLinear().range([0, 160]).domain([0, 100]);
 
         //console.log(vis.legendScale.domain())
 
-        vis.legendAxis = d3.axisBottom()
-            .scale(vis.legendScale)
-            .ticks(3)
+        vis.legendAxis = d3.axisBottom().scale(vis.legendScale).ticks(3);
 
-        vis.legendAxisGroup = vis.legend.append("g")
-            .attr("class", "axis axis--legend");
+        vis.legendAxisGroup = vis.legend
+            .append('g')
+            .attr('class', 'axis axis--legend');
 
-        vis.legend.selectAll().data(vis.colors)
+        vis.legend
+            .selectAll()
+            .data(vis.colors)
             .enter()
             .append('rect')
             .attr('width', 40)
             .attr('height', 20)
             .attr('x', (d, i) => i * 40)
             .attr('y', -20)
-            .attr('fill', d => d)
+            .attr('fill', (d) => d);
 
-        vis.legendAxisGroup.call(vis.legendAxis)
+        vis.legendAxisGroup.call(vis.legendAxis);
 
         // tooltip
-        vis.tooltip = d3.select("body").append('div')
-            .attr('class', "tooltip")
-            .attr('id', 'mapTooltip')
-
+        vis.tooltip = d3
+            .select('body')
+            .append('div')
+            .attr('class', 'tooltip')
+            .attr('id', 'mapTooltip');
 
         //d3.geoOrthographic()
-        vis.rotate = [0, 0]
+        vis.rotate = [0, 0];
         vis.zoom = vis.height / 600;
 
-
-        vis.projection = d3.geoOrthographic() // d3.geoStereographic()
+        vis.projection = d3
+            .geoOrthographic() // d3.geoStereographic()
             .translate([vis.width / 2, vis.height / 2])
             .scale(249.5 * vis.zoom) // 249.5 is default. so multiply that by your zoom
             .rotate(vis.rotate);
 
         // path provider
-        vis.path = d3.geoPath()
-            .projection(vis.projection);
+        vis.path = d3.geoPath().projection(vis.projection);
 
-
-        vis.svg.append("path")
-            .datum(
-                {type: "Sphere"}
-            )
-            .attr("class", "graticule")
+        vis.svg
+            .append('path')
+            .datum({ type: 'Sphere' })
+            .attr('class', 'graticule')
             .attr('fill', '#ADDEFF')
-            .attr("stroke", "rgba(129,129,129,0.35)")
-            .attr("d", vis.path);
+            .attr('stroke', 'rgba(129,129,129,0.35)')
+            .attr('d', vis.path);
 
-
-        vis.svg.append("path")
+        vis.svg
+            .append('path')
             .datum(d3.geoGraticule())
-            .attr("class", "graticule")
+            .attr('class', 'graticule')
             .attr('fill', '#ADDEFF')
-            .attr("stroke", "rgba(129,129,129,0.35)")
-            .attr("d", vis.path);
-
+            .attr('stroke', 'rgba(129,129,129,0.35)')
+            .attr('d', vis.path);
 
         // Convert TopoJSON to GeoJSON (target object = 'states')
-        let world = topojson.feature(vis.geoData, vis.geoData.objects.countries).features
+        let world = topojson.feature(
+            vis.geoData,
+            vis.geoData.objects.countries
+        ).features;
 
-        vis.countries = vis.svg.selectAll(".country")
+        vis.countries = vis.svg
+            .selectAll('.country')
             .data(world)
-            .enter().append("path")
+            .enter()
+            .append('path')
             .attr('class', 'country')
-            .attr("d", vis.path)
+            .attr('d', vis.path);
 
         //
-        let m0,
-            o0;
+        let m0, o0;
+        var scrollSpeed = 50;
+        var current = 0;
+        const bgscroll = () => {
+            current++;
+            vis.projection.rotate([vis.legendScale(current), 0]);
+            vis.svg.selectAll('path').attr('d', vis.path);
+        };
 
+        let temp = setInterval(bgscroll, scrollSpeed);
 
         vis.svg.call(
-            d3.drag()
-                .on("start", function (event) {
-
+            d3
+                .drag()
+                .on('start', function (event) {
                     let lastRotationParams = vis.projection.rotate();
                     m0 = [event.x, event.y];
                     o0 = [-lastRotationParams[0], -lastRotationParams[1]];
                 })
-                .on("drag", function (event) {
+                .on('drag', function (event) {
+                    clearInterval(temp);
                     if (m0) {
                         let m1 = [event.x, event.y],
                             o1 = [o0[0] + (m0[0] - m1[0]) / 4, o0[1] + (m1[1] - m0[1]) / 4];
@@ -137,33 +159,44 @@ class MapVis {
 
                     // Update the map
                     vis.path = d3.geoPath().projection(vis.projection);
-                    d3.selectAll(".country").attr("d", vis.path)
-                    d3.selectAll(".graticule").attr("d", vis.path)
+                    d3.selectAll('.country').attr('d', vis.path);
+                    d3.selectAll('.graticule').attr('d', vis.path);
 
                     // BONUS
-                    d3.selectAll(".sharks")
-                        .attr('cx', d => vis.projection([d.longitude, d.latitude])[0])
-                        .attr('cy', d => vis.projection([d.longitude, d.latitude])[1])
+                    d3.selectAll('.sharks')
+                        .attr('cx', (d) => vis.projection([d.longitude, d.latitude])[0])
+                        .attr('cy', (d) => vis.projection([d.longitude, d.latitude])[1]);
 
                     // BONUS
                     d3.selectAll('.connection')
-                        .attr("x1", function (d) {
-                            return vis.projection([vis.sharksData.nodes[d.source].longitude, vis.sharksData.nodes[d.source].latitude])[0];
+                        .attr('x1', function (d) {
+                            return vis.projection([
+                                vis.sharksData.nodes[d.source].longitude,
+                                vis.sharksData.nodes[d.source].latitude,
+                            ])[0];
                         })
-                        .attr("y1", function (d) {
-                            return vis.projection([vis.sharksData.nodes[d.source].longitude, vis.sharksData.nodes[d.source].latitude])[1];
+                        .attr('y1', function (d) {
+                            return vis.projection([
+                                vis.sharksData.nodes[d.source].longitude,
+                                vis.sharksData.nodes[d.source].latitude,
+                            ])[1];
                         })
-                        .attr("x2", function (d) {
-                            return vis.projection([vis.sharksData.nodes[d.target].longitude, vis.sharksData.nodes[d.target].latitude])[0];
+                        .attr('x2', function (d) {
+                            return vis.projection([
+                                vis.sharksData.nodes[d.target].longitude,
+                                vis.sharksData.nodes[d.target].latitude,
+                            ])[0];
                         })
-                        .attr("y2", function (d) {
-                            return vis.projection([vis.sharksData.nodes[d.target].longitude, vis.sharksData.nodes[d.target].latitude])[1];
+                        .attr('y2', function (d) {
+                            return vis.projection([
+                                vis.sharksData.nodes[d.target].longitude,
+                                vis.sharksData.nodes[d.target].latitude,
+                            ])[1];
                         });
                 })
         );
 
-        vis.wrangleData()
-
+        vis.wrangleData();
     }
 
     wrangleData() {
@@ -171,59 +204,83 @@ class MapVis {
 
         // create random data structure with information for each land
         vis.countryInfo = {};
-        vis.geoData.objects.countries.geometries.forEach(d => {
-            let randomCountryValue = Math.random() * 4
+        vis.geoData.objects.countries.geometries.forEach((d) => {
+            let randomCountryValue = Math.random() * 4;
             vis.countryInfo[d.properties.name] = {
                 name: d.properties.name,
                 category: 'category_' + Math.floor(randomCountryValue),
                 color: vis.colors[Math.floor(randomCountryValue)],
-                value: randomCountryValue / 4 * 100
-            }
-        })
+                value: (randomCountryValue / 4) * 100,
+            };
+        });
 
-        vis.updateVis()
+        vis.updateVis();
     }
-
 
     updateVis() {
         let vis = this;
 
-        console.log(vis.countryInfo)
+        console.log(vis.countryInfo);
 
         vis.countries
             .on('mouseover', function (event, d) {
                 d3.select(this)
-                    .attr("fill", 'rgba(181,0,0,0.48)')
-                    .attr("stroke", 'darkred')
+                    .attr('fill', 'rgba(181,0,0,0.48)')
+                    .attr('stroke', 'darkred');
 
-                console.log(d)
+                console.log(d);
                 vis.tooltip
-                    .style("opacity", 1)
-                    .style("left", event.pageX + 20 + "px")
-                    .style("top", event.pageY + "px")
-                    .html(`
+                    .style('opacity', 1)
+                    .style('left', event.pageX + 20 + 'px')
+                    .style('top', event.pageY + 'px').html(`
                         <div style="border: thin solid grey; border-radius: 5px; background: lightgrey; padding: 20px">
-                            <h3>${d.properties.name}<h3>
-                            <h4> Name: ${vis.countryInfo[d.properties.name].name}</h4>     
-                            <h4> Category: ${vis.countryInfo[d.properties.name].category}</h4>       
-                            <h4> Color: ${vis.countryInfo[d.properties.name].color}</h4>   
-                            <h4> Value: ${vis.countryInfo[d.properties.name].value}</h4>               
+                            <h3>${d.properties.name}<h3>      
+                         
+                            <h4> Fatal Attacks: ${
+                    vis.data.filter(
+                        (item) =>
+                            item.fatal === 'Yes' &&
+                            item.country?.toLowerCase() ===
+                            vis.countryInfo[
+                                d.properties.name
+                                ].name.toLowerCase()
+                    ).length
+                }</h4>    
+                             <h4>Non Fatal Attacks: ${
+                    vis.data.filter(
+                        (item) =>
+                            item.fatal === 'No' &&
+                            item.country?.toLowerCase() ===
+                            vis.countryInfo[
+                                d.properties.name
+                                ].name.toLowerCase()
+                    ).length
+                }</h4>  
+                                 <h4> Total Attacks: ${
+                    vis.data.filter(
+                        (item) =>
+                            (item.fatal === 'Yes' ||
+                                item.fatal === 'No') &&
+                            item.country?.toLowerCase() ===
+                            vis.countryInfo[
+                                d.properties.name
+                                ].name.toLowerCase()
+                    ).length
+                }</h4>                
                         </div>`);
-
             })
             .on('mouseout', function (event, d) {
                 d3.select(this)
-                    .attr('fill', d => vis.countryInfo[d.properties.name].color)
-                    .attr("stroke", 'transparent')
+                    .attr('fill', (d) => vis.countryInfo[d.properties.name].color)
+                    .attr('stroke', 'transparent');
 
                 vis.tooltip
-                    .style("opacity", 0)
-                    .style("left", 0 + "px")
-                    .style("top", 0 + "px")
+                    .style('opacity', 0)
+                    .style('left', 0 + 'px')
+                    .style('top', 0 + 'px');
             })
             .transition()
             .duration(500)
-            .attr('fill', d => vis.countryInfo[d.properties.name].color)
-
+            .attr('fill', (d) => vis.countryInfo[d.properties.name].color);
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,55 +1,49 @@
-console.log('getting started on the final project!')
+console.log('getting started on the final project!');
 
 let promises = [
-    d3.csv('data/activities-data.csv', d3.autoType),
-    d3.csv('data/attacks-fatalities.csv', d3.autoType),
-    d3.csv('data/odds.csv', d3.autoType),
-    d3.json('data/sharks.json'),
-    d3.json('data/world-110m.json'),
-    d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json'),
-    d3.json('https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json')
-
+  d3.csv('data/activities-data.csv', d3.autoType),
+  d3.csv('data/attacks-fatalities.csv', d3.autoType),
+  d3.csv('data/odds.csv', d3.autoType),
+  d3.json('data/sharks.json'),
+  d3.json('data/world-110m.json'),
+  d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json'),
+  d3.json('https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json'),
+  d3.csv('data/shark-encounters.csv', d3.autoType),
 ];
 
 Promise.all(promises)
-    .then(function (data) {
-        createVis(data)
-    })
-    .catch(function (err) {
-        console.log(err)
-    });
-
+  .then(function (data) {
+    createVis(data);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
 
 function createVis(data) {
-    let barChartData = data[0]
-    let lineChartData = data[1]
+  let barChartData = data[0];
+  let lineChartData = data[1];
 
-    let geoData = data[5]
-    let sharksData = data[3]
+  let geoData = data[5];
+  let sharksData = data[3];
 
-    console.log(data)
+  // Instantiate the visualizations
+  barChart = new BarChart(
+    'activities-bar-chart',
+    barChartData,
+    'What were people doing when they encountered a shark?'
+  );
+  //lineChart = new LineChart('line-plot-div', lineChartData);
 
-    // Instantiate the visualizations
-    barChart = new BarChart('activities-bar-chart', barChartData, 'What were people doing when they encountered a shark?');
-    //lineChart = new LineChart('line-plot-div', lineChartData);
+  myMapVis = new MapVis('map-div', sharksData, geoData, data[7]);
 
-    myMapVis = new MapVis('map-div', sharksData, geoData)
-
-//    lollipopChart = new LollipopChart('foo-div', lollipopChartData);
-
+  //    lollipopChart = new LollipopChart('foo-div', lollipopChartData);
 }
-
 
 function brushed() {
-
-    // Gets the extent of the current brush
-    let selectionRange = d3.brushSelection(d3.select('.brush').node());
-
-    //
-
+  // Gets the extent of the current brush
+  let selectionRange = d3.brushSelection(d3.select('.brush').node());
 
 }
-
 
 //d3.csv('data/activities-data.csv', d3.autoType).then(function(data) {
 
@@ -58,10 +52,8 @@ function brushed() {
 //    // Instantiate the visualizations
 //    barChart = new BarChart('activities-bar-chart', data)
 
-    //lineChart = new LineChart('line-plot-div', data)
+//lineChart = new LineChart('line-plot-div', data)
 
 //    console.log(data)
 
-
 //})
-
